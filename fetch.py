df3b5d41dc50f30584df8503c8875dd4ced68111
amassock <ajmassock@gmail.com>
2023-05-10 20:34:23 -0500

    write data into data folder instead of root

--- fetch.py
+++ fetch.py
@@ -10,10 +10,12 @@ def fetch_data():
     res = requests.get(url)
     response = res.json()
     json_data = json.dumps(response, indent=4)
-    bill = Path('./bill_data.json')
+    bill = Path('./data/bill_data.json')
 
     if not bill:
-        open('bill_data.json', 'x')
+        open('/data/bill_data.json', 'x')
     else:
-        with open('bill_data.json', 'w') as outfile:
+        with open('./data/bill_data.json', 'w') as outfile:
             outfile.write(json_data)
+
+fetch_data()
